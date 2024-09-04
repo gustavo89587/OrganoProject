1942e9e574e1d0bca932ce7503c17d52620315b8
Gustavo O. de Carvalho <161786778+gustavo89587@users.noreply.github.com>
2024-09-04 17:21:50 -0300

    Add files via upload

---
+++ Banner.js
@@ -0,0 +1,11 @@
+import  './Banner.css'
+
+function Banner() {
+   return (
+        <header className="Banner">
+           <img src="/imagens/banner.png"></img>
+        </header>
+   )
+}
+
+export default Banner
--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './ListaSuspensa.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const ListaSuspensa = (props) => {
+    console.log(props.itens)
+    
+    return(
+        <div>
+            <label>{props.label}</label>
+            <select>
+                {props.itens.map(item => <option>{item}</option>)}
+            </select>
+        </div>
+    )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+
+export default ListaSuspensa
